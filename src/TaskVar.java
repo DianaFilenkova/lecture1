33ef17a25480f4d758de123dea53b6a1a267b8a3
diana <dianafilenkova95@gmail.com>
2020-02-01 20:09:53 +0000

    Initial commit

---
+++ src/TaskVar.java
@@ -0,0 +1,18 @@
+public class TaskVar {
+    public static void main (String[] args) {
+        int numAdd;
+        numAdd = 125/25;
+        System.out.println(numAdd+" = 125/25");
+
+        int numAdd1;
+        numAdd1 = 125+25;
+        System.out.println(numAdd1+" =125+25");
+
+        int numAdd2;
+        int num1 = 125;
+        int num2 = 25;
+        numAdd2 = num1 + num2;
+        System.out.println(numAdd2+" = "+num1+ " + "+num2);
+
+    }
+}
